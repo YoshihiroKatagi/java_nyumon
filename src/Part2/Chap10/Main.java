7a500f72e61ed913e4d2908ef05850828d0e1e49
katagi <katagiyoshihiro@gmail.com>
2022-10-13 13:10:28 +0900

    Chap10-3

---
+++ src/Part2/Chap10/Main.java
@@ -0,0 +1,10 @@
+package Part2.Chap10;
+
+public final class Main {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.run();
+        SuperHero sh = new SuperHero();
+        sh.run();
+    }
+}
--- src/Part2/Chap9/Main.java
+++ src/Part2/Chap9/Main.java
@@ -5,15 +5,12 @@ public class Main {
         Sword s = new Sword();
         s.name = "炎の剣";
         s.damage = 10;
-        Hero h1 = new Hero();
-        h1.name = "ミナト";
-        h1.hp = 100;
+        Hero h1 = new Hero("ミナト");
         h1.sword = s;
         System.out.println("現在の武器は" + h1.sword.name);
         h1.attack();
+//        Hero h2 = new Hero("アサカ");
         Hero h2 = new Hero();
-        h2.name = "アサカ";
-        h2.hp = 100;
         Wizard w = new Wizard();
         w.name = "スガワラ";
         w.hp = 50;
