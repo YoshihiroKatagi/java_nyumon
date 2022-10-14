fa5ff6ab4a08be02038691c3b09389c0819eb665
YoshihiroKatagi <katagiyoshihiro@gmail.com>
2022-10-14 17:03:57 +0900

    Chap11

---
+++ src/Part2/Chap11/Book.java
@@ -0,0 +1,12 @@
+package Part2.Chap11;
+
+public class Book extends TangibleAsset {
+    String isbn;
+
+    public Book(String name, int price, String color, String isbn) {
+        super(name, price, isbn);
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() { return this.isbn; }
+}
---
+++ src/Part2/Chap11/Thing.java
@@ -0,0 +1,6 @@
+package Part2.Chap11;
+
+public interface Thing {
+    double getWeight();
+    void setWeight(double weight);
+}
