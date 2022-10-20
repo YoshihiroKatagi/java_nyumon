e91a1cf1aa8d96e1d7084f66d68abdf9eea8cc50
YoshihiroKatagi <katagiyoshihiro@gmail.com>
2022-10-20 11:15:11 +0900

    complete

--- src/Part3/Chap18/Main.java
+++ src/Part3/Chap18/Main.java
@@ -3,6 +3,9 @@ package Part3.Chap18;
 import java.io.*;
 import java.io.InputStream;
 import java.net.URL;
+import java.sql.*;
+import java.awt.FlowLayout;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -14,7 +17,7 @@ public class Main {
 //            input = fr.read();
 //        }
 //        fr.close();
-//
+
         // code 18-2
 //        FileWriter fw = new FileWriter("data.txt");
 //        fw.write('そ');
@@ -24,13 +27,32 @@ public class Main {
 //        fw.close();
 
         // code 18-3
-        URL u = new URL("https://book.impress.co.jp/");
-        InputStream is = u.openStream();
-        int i = is.read();
-        while (i != -1) {
-            char c = (char)i;
-            System.out.print(c);
-            i = is.read();
-        }
+//        URL u = new URL("https://book.impress.co.jp/");
+//        InputStream is = u.openStream();
+//        int i = is.read();
+//        while (i != -1) {
+//            char c = (char)i;
+//            System.out.print(c);
+//            i = is.read();
+//        }
+
+        // code 18-4
+//        Class.forName("org.h2.Driver");
+//        String dburl = "jdbc:h2:~/test";
+//        String sql = "INSERT INTO EMPLOYEES(name) VALUES('aoki')";
+//        Connection conn = DriverManager.getConnection(dburl);
+//        conn.createStatement().executeUpdate(sql);
+//        conn.close();
+
+        // code 18-5
+        JFrame frame = new JFrame("はじめてのGUI");
+        JLabel label = new JLabel("Hello World!");
+        JButton button = new JButton("押してね");
+        frame.getContentPane().setLayout(new FlowLayout());
+        frame.getContentPane().add(label);
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
     }
 }
